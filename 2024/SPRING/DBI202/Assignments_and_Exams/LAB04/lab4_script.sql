c1f069ddc3aef5d3c747c426681b49bb66ed1501
unknown <hoangziet22@gmail.com>
2024-03-03 10:00:12 +0700

    2024 March 3

--- 2024/SPRING/DBI202/Assignments_and_Exams/LAB04/lab4_script.sql
+++ 2024/SPRING/DBI202/Assignments_and_Exams/LAB04/lab4_script.sql
@@ -73,7 +73,8 @@ FROM tblLocation l
     INNER JOIN tblDepartment d ON d.depNum = dl.depNum
 WHERE d.depName = N'Phòng Nghiên cứu và phát triển';
 
---11.	Cho biết các dự án làm việc tại Tp. HCM. Thông tin yêu cầu: mã dự án, tên dự án, tên phòng ban chịu trách nhiệm dự án.
+--11.	Cho biết các dự án làm việc tại Tp. HCM. 
+--Thông tin yêu cầu: mã dự án, tên dự án, tên phòng ban chịu trách nhiệm dự án.
 SELECT p.proNum, p.proName, d.depName
 FROM tblProject p
     INNER JOIN tblLocation l ON p.locNum = l.locNum
@@ -183,13 +184,12 @@ WITH
     AS
     (
         SELECT
-            e.empSSN, e.empName, d.depName, SUM(w.workHours) as total_ews
+            e.empSSN, e.empName, SUM(w.workHours) as total_ews
         FROM tblEmployee e
             INNER JOIN tblWorksOn w ON e.empSSN = w.empSSN
             INNER JOIN tblDepartment d ON e.depNum = d.depNum
         GROUP BY e.empSSN,
-            e.empName,
-            d.depName
+            e.empName
     )
 SELECT *
 FROM ws
@@ -217,334 +217,395 @@ from ws);
 --23.	Cho biết những nhân viên nào lần đầu tiên tham gia dụ án. 
 --Thông tin yêu cầu: mã nhân viên, tên nhân viên, tên phòng ban của nhân viên
 WITH
-	pc
-	AS
-(SELECT e.empSSN,e.empName,d.depName,COUNT(p.proNum) as total_pro
-FROM tblEmployee e
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-INNER JOIN tblProject p ON p.depNum = d.depNum
-GROUP BY e.empSSN,e.empName,d.depName)
-SELECT empSSN,empName,depName
+    pc
+    AS
+    (
+        SELECT e.empSSN, e.empName, d.depName, COUNT(p.proNum) as total_pro
+        FROM tblEmployee e
+            INNER JOIN tblDepartment d ON e.depNum = d.depNum
+            INNER JOIN tblProject p ON p.depNum = d.depNum
+        GROUP BY e.empSSN,e.empName,d.depName
+    )
+SELECT empSSN, empName, depName
 FROM pc
-WHERE total_pro = 1; --Lần đầu tham gia dự án nên đếm số dự án tham gia là 1
+WHERE total_pro = 1;
+--Lần đầu tham gia dự án nên đếm số dự án tham gia là 1
 
 
 -- 24.	Cho biết những nhân viên nào lần thứ hai tham gia dụ án. 
 -- Thông tin yêu cầu: mã nhân viên, tên nhân viên, tên phòng ban của nhân viên
 WITH
-	pc
-	AS
-(SELECT e.empSSN,e.empName,d.depName,COUNT(p.proNum) as total_pro
-FROM tblEmployee e
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-INNER JOIN tblProject p ON p.depNum = d.depNum
-GROUP BY e.empSSN,e.empName,d.depName)
-SELECT empSSN,empName,depName
+    pc
+    AS
+    (
+        SELECT e.empSSN, e.empName, d.depName, COUNT(p.proNum) as total_pro
+        FROM tblEmployee e
+            INNER JOIN tblDepartment d ON e.depNum = d.depNum
+            INNER JOIN tblProject p ON p.depNum = d.depNum
+        GROUP BY e.empSSN,e.empName,d.depName
+    )
+SELECT empSSN, empName, depName
 FROM pc
 WHERE total_pro = 2;
 
 --25.	Cho biết những nhân viên nào tham gia tối thiểu hai dụ án. 
 --Thông tin yêu cầu: mã nhân viên, tên nhân viên, tên phòng ban của nhân viên
 WITH
-	pc
-	AS
-(SELECT e.empSSN,e.empName,d.depName,COUNT(p.proNum) as total_pro
-FROM tblEmployee e
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-INNER JOIN tblProject p ON p.depNum = d.depNum
-GROUP BY e.empSSN,e.empName,d.depName)
-SELECT empSSN,empName,depName
+    pc
+    AS
+    (
+        SELECT e.empSSN, e.empName, d.depName, COUNT(p.proNum) as total_pro
+        FROM tblEmployee e
+            INNER JOIN tblDepartment d ON e.depNum = d.depNum
+            INNER JOIN tblProject p ON p.depNum = d.depNum
+        GROUP BY e.empSSN,e.empName,d.depName
+    )
+SELECT empSSN, empName, depName
 FROM pc
 WHERE total_pro >= 2;
 
 
 --26.	Cho biết số lượng thành viên của mỗi dự án. 
 --Thông tin yêu cầu: mã dự án, tên dự án, số lượng thành viên
-SELECT p.proNum,p.proName,COUNT(e.empSSN) as total_emp
+SELECT p.proNum, p.proName, COUNT(e.empSSN) as total_emp
 FROM tblProject p
-INNER JOIN tblEmployee e ON e.depNum = p.depNum
+    INNER JOIN tblEmployee e ON e.depNum = p.depNum
 GROUP BY p.proNum,p.proName;
 
 --27.	Cho biết tổng số giờ làm của mỗi dự án. 
 --Thông tin yêu cầu: mã dự án, tên dự án, tổng số giờ làm
-SELECT p.proNum,p.proName,SUM(w.workHours) as p_ws
-FROM tblProject p 
-INNER JOIN tblWorksOn w ON p.proNum = w.proNum
+SELECT p.proNum, p.proName, SUM(w.workHours) as p_ws
+FROM tblProject p
+    INNER JOIN tblWorksOn w ON p.proNum = w.proNum
 GROUP BY p.proNum,p.proName;
 
 --28.	Cho biết dự án nào có số lượng thành viên là ít nhất. 
 --Thông tin yêu cầu: mã dự án, tên dự án, số lượng thành viên
-WITH p_ec AS (
-  SELECT p.proNum,
-         p.proName,
-         COUNT(e.empSSN) AS total_emp
-  FROM tblProject p
-  INNER JOIN tblEmployee e ON e.depNum = p.depNum
-  GROUP BY p.proNum, p.proName
-)
+WITH
+    p_ec
+    AS
+    (
+        SELECT p.proNum,
+            p.proName,
+            COUNT(e.empSSN) AS total_emp
+        FROM tblProject p
+            INNER JOIN tblEmployee e ON e.depNum = p.depNum
+        GROUP BY p.proNum, p.proName
+    )
 SELECT proNum,
-       proName,
-       total_emp
+    proName,
+    total_emp
 FROM p_ec
-WHERE total_emp = (SELECT MIN(total_emp) FROM p_ec);
+WHERE total_emp = (SELECT MIN(total_emp)
+FROM p_ec);
 
 --29.	Cho biết dự án nào có số lượng thành viên là nhiều nhất. 
 --Thông tin yêu cầu: mã dự án, tên dự án, số lượng thành viên
-WITH p_ec AS (
-  SELECT p.proNum,
-         p.proName,
-         COUNT(e.empSSN) AS total_emp
-  FROM tblProject p
-  INNER JOIN tblEmployee e ON e.depNum = p.depNum
-  GROUP BY p.proNum, p.proName
-)
+WITH
+    p_ec
+    AS
+    (
+        SELECT p.proNum,
+            p.proName,
+            COUNT(e.empSSN) AS total_emp
+        FROM tblProject p
+            INNER JOIN tblEmployee e ON e.depNum = p.depNum
+        GROUP BY p.proNum, p.proName
+    )
 SELECT proNum,
-       proName,
-       total_emp
+    proName,
+    total_emp
 FROM p_ec
-WHERE total_emp = (SELECT MAX(total_emp) FROM p_ec);
+WHERE total_emp = (SELECT MAX(total_emp)
+FROM p_ec);
 
 --30.	Cho biết dự án nào có tổng số giờ làm là ít nhất. 
 --Thông tin yêu cầu: mã dự án, tên dự án, tổng số giờ làm
-WITH 
-	pws
-	AS
-(SELECT p.proNum,p.proName,SUM(w.workHours) as total_ws
-FROM tblProject p 
-INNER JOIN tblWorksOn w ON p.proNum = w.proNum
-GROUP BY p.proNum,p.proName)
+WITH
+    pws
+    AS
+    (
+        SELECT p.proNum, p.proName, SUM(w.workHours) as total_ws
+        FROM tblProject p
+            INNER JOIN tblWorksOn w ON p.proNum = w.proNum
+        GROUP BY p.proNum,p.proName
+    )
 SELECT *
 FROM pws
-WHERE total_ws = (SELECT MIN(total_ws) FROM pws);
+WHERE total_ws = (SELECT MIN(total_ws)
+FROM pws);
 
 --31.	Cho biết dự án nào có tổng số giờ làm là nhiều nhất. 
 --Thông tin yêu cầu: mã dự án, tên dự án, tổng số giờ làm
-WITH 
-	pws
-	AS
-(SELECT p.proNum,p.proName,SUM(w.workHours) as total_ws
-FROM tblProject p 
-INNER JOIN tblWorksOn w ON p.proNum = w.proNum
-GROUP BY p.proNum,p.proName)
+WITH
+    pws
+    AS
+    (
+        SELECT p.proNum, p.proName, SUM(w.workHours) as total_ws
+        FROM tblProject p
+            INNER JOIN tblWorksOn w ON p.proNum = w.proNum
+        GROUP BY p.proNum,p.proName
+    )
 SELECT *
 FROM pws
-WHERE total_ws = (SELECT MAX(total_ws) FROM pws);
+WHERE total_ws = (SELECT MAX(total_ws)
+FROM pws);
 
 --32.	Cho biết số lượng phòng ban làm việc theo mỗi nơi làm việc. 
 --Thông tin yêu cầu: tên nơi làm việc, số lượng phòng ban
-SELECT l.locName,COUNT(dl.depNum) AS total_dep
+SELECT l.locName, COUNT(dl.depNum) AS total_dep
 FROM tblLocation l
-INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
+    INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
 GROUP BY l.locName;
 
 
 --33.	Cho biết số lượng chỗ làm việc theo mỗi phòng ban. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng chỗ làm việc
-SELECT d.depNum,d.depName,COUNT(dl.depNum) AS total_dep
+SELECT d.depNum, d.depName, COUNT(dl.depNum) AS total_dep
 FROM tblDepartment d
-INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
+    INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
 GROUP BY d.depNum,d.depName;
 
 --34.	Cho biết phòng ban nào có nhiều chỗ làm việc nhất. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng chỗ làm việc
 WITH
-	lc_eachdep
-	AS
-(SELECT d.depNum,d.depName,COUNT(dl.depNum) AS total_dep
-FROM tblDepartment d
-INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
-GROUP BY d.depNum,d.depName)
-SELECT * 
+    lc_eachdep
+    AS
+    (
+        SELECT d.depNum, d.depName, COUNT(dl.depNum) AS total_dep
+        FROM tblDepartment d
+            INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
+        GROUP BY d.depNum,d.depName
+    )
+SELECT *
 FROM lc_eachdep
-WHERE total_dep = (SELECT MAX(total_dep) FROM lc_eachdep);
+WHERE total_dep = (SELECT MAX(total_dep)
+FROM lc_eachdep);
 
 --35.	Cho biết phòng ban nào có ít chỗ làm việc nhất. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng chỗ làm việc
 WITH
-	lc_eachdep
-	AS
-(SELECT d.depNum,d.depName,COUNT(dl.depNum) AS total_dep
-FROM tblDepartment d
-INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
-GROUP BY d.depNum,d.depName)
-SELECT * 
+    lc_eachdep
+    AS
+    (
+        SELECT d.depNum, d.depName, COUNT(dl.depNum) AS total_dep
+        FROM tblDepartment d
+            INNER JOIN tblDepLocation dl ON d.depNum = dl.depNum
+        GROUP BY d.depNum,d.depName
+    )
+SELECT *
 FROM lc_eachdep
-WHERE total_dep = (SELECT MIN(total_dep) FROM lc_eachdep);
+WHERE total_dep = (SELECT MIN(total_dep)
+FROM lc_eachdep);
 
 --36.	Cho biết địa điểm nào có nhiều phòng ban làm việc nhất. 
 --Thông tin yêu cầu: tên nơi làm việc, số lượng phòng ban
-WITH 
-	dep_eachl
-	AS
-(SELECT l.locName,COUNT(dl.depNum) AS total_dep
-FROM tblLocation l
-INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
-GROUP BY l.locName)
+WITH
+    dep_eachl
+    AS
+    (
+        SELECT l.locName, COUNT(dl.depNum) AS total_dep
+        FROM tblLocation l
+            INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
+        GROUP BY l.locName
+    )
 SELECT *
 FROM dep_eachl
-WHERE total_dep = (SELECT MAX(total_dep) FROM dep_eachl) ;
+WHERE total_dep = (SELECT MAX(total_dep)
+FROM dep_eachl)
+;
 
 --37.	Cho biết địa điểm nào có ít phòng ban làm việc nhất. 
 --Thông tin yêu cầu: tên nơi làm việc, số lượng phòng ban
-WITH 
-	dep_eachl
-	AS
-(SELECT l.locName,COUNT(dl.depNum) AS total_dep
-FROM tblLocation l
-INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
-GROUP BY l.locName)
+WITH
+    dep_eachl
+    AS
+    (
+        SELECT l.locName, COUNT(dl.depNum) AS total_dep
+        FROM tblLocation l
+            INNER JOIN tblDepLocation dl ON l.locNum = dl.locNum
+        GROUP BY l.locName
+    )
 SELECT *
 FROM dep_eachl
-WHERE total_dep = (SELECT MIN(total_dep) FROM dep_eachl) ;
+WHERE total_dep = (SELECT MIN(total_dep)
+FROM dep_eachl)
+;
 
 --38.	Cho biết nhân viên nào có nhiều người phụ thuộc nhất. 
 --Thông tin yêu cầu: mã số, họ tên nhân viên, số lượng người phụ thuộc
-WITH 
-	dpc_eache 
-	AS
-(SELECT e.empSSN,e.empName,COUNT(*) as total_dp
-FROM tblEmployee e
-INNER JOIN tblDependent dp ON e.empSSN=dp.empSSN
-GROUP BY e.empSSN,e.empName)
+WITH
+    dpc_eache
+    AS
+    (
+        SELECT e.empSSN, e.empName, COUNT(*) as total_dp
+        FROM tblEmployee e
+            INNER JOIN tblDependent dp ON e.empSSN=dp.empSSN
+        GROUP BY e.empSSN,e.empName
+    )
 SELECT *
 FROM dpc_eache
-WHERE total_dp = (SELECT MAX(total_dp) FROM dpc_eache);
+WHERE total_dp = (SELECT MAX(total_dp)
+FROM dpc_eache);
 
 --39.	Cho biết nhân viên nào có ít người phụ thuộc nhất. 
 --Thông tin yêu cầu: mã số, họ tên nhân viên, số lượng người phụ thuộc
-WITH 
-	dpc_eache 
-	AS
-(SELECT e.empSSN,e.empName,COUNT(*) as total_dp
-FROM tblEmployee e
-INNER JOIN tblDependent dp ON e.empSSN=dp.empSSN
-GROUP BY e.empSSN,e.empName)
+WITH
+    dpc_eache
+    AS
+    (
+        SELECT e.empSSN, e.empName, COUNT(*) as total_dp
+        FROM tblEmployee e
+            INNER JOIN tblDependent dp ON e.empSSN=dp.empSSN
+        GROUP BY e.empSSN,e.empName
+    )
 SELECT *
 FROM dpc_eache
-WHERE total_dp = (SELECT MIN(total_dp) FROM dpc_eache);
+WHERE total_dp = (SELECT MIN(total_dp)
+FROM dpc_eache);
 
 
 --40.	Cho biết nhân viên nào không có người phụ thuộc.
 --Thông tin yêu cầu: mã số nhân viên, họ tên nhân viên, tên phòng ban của nhân viên
-SELECT e.empSSN,e.empName,d.depName
+SELECT e.empSSN, e.empName, d.depName
 FROM tblEmployee e
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-WHERE e.empSSN NOT IN (SELECT empSSN FROM tblDependent);
+    INNER JOIN tblDepartment d ON e.depNum = d.depNum
+WHERE e.empSSN NOT IN (SELECT empSSN
+FROM tblDependent);
 
 --41.	Cho biết phòng ban nào không có người phụ thuộc. 
 --Thông tin yêu cầu: mã số phòng ban, tên phòng ban
-SELECT DISTINCT d.depNum,d.depName
-FROM tblDepartment d 
-INNER JOIN tblEmployee e ON d.depNum = e.depNum
-WHERE e.empSSN NOT IN (SELECT empSSN FROM tblDependent);
+SELECT DISTINCT d.depNum, d.depName
+FROM tblDepartment d
+    INNER JOIN tblEmployee e ON d.depNum = e.depNum
+WHERE e.empSSN NOT IN (SELECT empSSN
+FROM tblDependent);
 
 --42.	Cho biết những nhân viên nào chưa hề tham gia vào bất kỳ dự án nào. 
 --Thông tin yêu cầu: mã số, tên nhân viên, tên phòng ban của nhân viên
 WITH
-	emp_d_p
-	AS
-(SELECT e.empSSN,e.empName,d.depName,p.proNum
-FROM tblEmployee e 
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-LEFT JOIN tblProject p ON d.depNum = p.depNum)
-SELECT empSSN,empName,depName
+    emp_d_p
+    AS
+    (
+        SELECT e.empSSN, e.empName, d.depName, p.proNum
+        FROM tblEmployee e
+            INNER JOIN tblDepartment d ON e.depNum = d.depNum
+            LEFT JOIN tblProject p ON d.depNum = p.depNum
+    )
+SELECT empSSN, empName, depName
 FROM emp_d_P
 WHERE proNum is NULL;
 
 --43.	Cho biết phòng ban không có nhân viên nào tham gia (bất kỳ) dự án. 
 --Thông tin yêu cầu: mã số phòng ban, tên phòng ban
-SELECT d.depNum,d.depName
+SELECT d.depNum, d.depName
 FROM tblDepartment d
-LEFT JOIN tblProject p ON d.depNum = p.depNum
+    LEFT JOIN tblProject p ON d.depNum = p.depNum
 WHERE p.proNum IS NULL;
 
 --44.	Cho biết phòng ban không có nhân viên nào tham gia vào dự án có tên là ProjectA. 
 --Thông tin yêu cầu: mã số phòng ban, tên phòng ban
-SELECT d.depNum,d.depName,p.proName
+SELECT DISTINCT d.depNum, d.depName
 FROM tblDepartment d
-LEFT JOIN tblProject p ON d.depNum = p.depNum
-WHERE p.proName = 'ProjectA' OR p.proNum IS NULL;
+    LEFT JOIN tblProject p ON d.depNum = p.depNum
+WHERE d.depNum != (SELECT depNum
+FROM tblProject
+WHERE proName = 'ProjectA');
+
 
 --45.	Cho biết số lượng dự án được quản lý theo mỗi phòng ban. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng dự án
-SELECT d.depNum,d.depName,COUNT(p.proNum) as total_p
+SELECT d.depNum, d.depName, COUNT(p.proNum) as total_p
 FROM tblDepartment d
-LEFT JOIN tblProject p ON d.depNum = p.depNum
+    LEFT JOIN tblProject p ON d.depNum = p.depNum
 GROUP BY d.depNum,d.depName;
 
 --46.	Cho biết phòng ban nào quản lý it dự án nhất. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng dự án
-WITH 
-	totalp_eachd
-	AS
-(SELECT d.depNum,d.depName,COUNT(p.proNum) as total_p
-FROM tblDepartment d
-LEFT JOIN tblProject p ON d.depNum = p.depNum
-GROUP BY d.depNum,d.depName)
+WITH
+    totalp_eachd
+    AS
+    (
+        SELECT d.depNum, d.depName, COUNT(p.proNum) as total_p
+        FROM tblDepartment d
+            LEFT JOIN tblProject p ON d.depNum = p.depNum
+        GROUP BY d.depNum,d.depName
+    )
 SELECT *
 FROM totalp_eachd
-WHERE total_p = (SELECT MIN(total_p) FROM totalp_eachd);
+WHERE total_p = (SELECT MIN(total_p)
+FROM totalp_eachd);
 
 --47.	Cho biết phòng ban nào quản lý nhiều dự án nhất. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng dự án
-WITH 
-	totalp_eachd
-	AS
-(SELECT d.depNum,d.depName,COUNT(p.proNum) as total_p
-FROM tblDepartment d
-LEFT JOIN tblProject p ON d.depNum = p.depNum
-GROUP BY d.depNum,d.depName)
+WITH
+    totalp_eachd
+    AS
+    (
+        SELECT d.depNum, d.depName, COUNT(p.proNum) as total_p
+        FROM tblDepartment d
+            LEFT JOIN tblProject p ON d.depNum = p.depNum
+        GROUP BY d.depNum,d.depName
+    )
 SELECT *
 FROM totalp_eachd
-WHERE total_p = (SELECT MAX(total_p) FROM totalp_eachd);
+WHERE total_p = (SELECT MAX(total_p)
+FROM totalp_eachd);
 
 --48.	Cho biết những phòng ban nào có nhiểu hơn 5 nhân viên đang quản lý dự án gì. 
 --Thông tin yêu cầu: mã phòng ban, tên phòng ban, số lượng nhân viên của phòng ban, tên dự án quản lý
-WITH 
+WITH
     totale_p_eachd
     AS
-(SELECT d.depNum,d.depName,COUNT(e.empSSN) as total_emp,p.proName
-FROM tblDepartment d
-INNER JOIN tblEmployee e ON d.depNum = e.depNum
-INNER JOIN tblProject p ON d.depNum = p.depNum
-GROUP BY d.depNum,d.depName,p.proName)
+    (
+        SELECT d.depNum, d.depName, COUNT(e.empSSN) as total_emp, p.proName
+        FROM tblDepartment d
+            INNER JOIN tblEmployee e ON d.depNum = e.depNum
+            INNER JOIN tblProject p ON d.depNum = p.depNum
+        GROUP BY d.depNum,d.depName,p.proName
+    )
 SELECT *
 FROM totale_p_eachd
 WHERE total_emp > 5;
 
 --49.	Cho biết những nhân viên thuộc phòng có tên là Phòng nghiên cứu, và không có người phụ thuộc. Thông tin yêu cầu: mã nhân viên,họ tên nhân viên
-SELECT e.empSSN,e.empName
+SELECT e.empSSN, e.empName
 FROM tblEmployee e
-INNER JOIN tblDepartment d ON e.depNum = d.depNum
-WHERE d.depName = N'Phòng Nghiên cứu và phát triển' AND e.empSSN NOT IN (SELECT empSSN FROM tblDependent);
+    INNER JOIN tblDepartment d ON e.depNum = d.depNum
+WHERE d.depName = N'Phòng Nghiên cứu và phát triển' AND e.empSSN NOT IN (SELECT empSSN
+    FROM tblDependent);
 
 --50.	Cho biết tổng số giờ làm của các nhân viên, mà các nhân viên này không có người phụ thuộc. 
 --Thông tin yêu cầu: mã nhân viên,họ tên nhân viên, tổng số giờ làm
-SELECT e.empSSN,e.empName,SUM(w.workHours) as totalws
+SELECT e.empSSN, e.empName, SUM(w.workHours) as totalws
 FROM tblWorksOn w
-INNER JOIN tblEmployee e ON w.empSSN = e.empSSN
-WHERE e.empSSN NOT IN (SELECT empSSN FROM tblDependent)
+    INNER JOIN tblEmployee e ON w.empSSN = e.empSSN
+WHERE e.empSSN NOT IN (SELECT empSSN
+FROM tblDependent)
 GROUP BY e.empSSN,e.empName;
 
 --51.	Cho biết tổng số giờ làm của các nhân viên, mà các nhân viên này có nhiều hơn 3 người phụ thuộc. 
 --Thông tin yêu cầu: mã nhân viên,họ tên nhân viên, số lượng người phụ thuộc, tổng số giờ làm
-WITH 
-	ws_dps_eache
-	AS
-(SELECT e.empSSN,e.empName,SUM(w.workHours) as totalws,COUNT(dp.depName) as total_dps
-FROM tblWorksOn w
-INNER JOIN tblEmployee e ON w.empSSN = e.empSSN
-INNER JOIN tblDependent dp ON e.empSSN = dp.empSSN
-GROUP BY e.empSSN,e.empName)
-SELECT *
-FROM ws_dps_eache
-WHERE total_dps>3;
+WITH
+    dps
+    AS
+    (
+        SELECT e.empSSN, e.empName, COUNT(dp.depName) as total_dps
+        FROM tblEmployee e
+            INNER JOIN tblDependent dp ON e.empSSN = dp.empSSN
+        GROUP BY e.empSSN,e.empName
+    )
+SELECT dps.empSSN, dps.empName, dps.total_dps, SUM(w.workHours) as total_ws
+FROM dps
+    INNER JOIN tblWorksOn w ON dps.empSSN = w.empSSN
+WHERE total_dps > 3
+GROUP BY dps.empSSN,dps.empName,dps.total_dps
 
 --52.	Cho biết tổng số giờ làm việc của các nhân viên hiện đang dưới quyền giám sát (bị quản lý bởi) của nhân viên Mai Duy An. 
 --Thông tin yêu cầu: mã nhân viên, họ tên nhân viên, tổng số giờ làm
-SELECT e1.empSSN,e1.empName,SUM(w.workHours) as ws
+SELECT e1.empSSN, e1.empName, SUM(w.workHours) as ws
 FROM tblEmployee e1
-INNER JOIN tblEmployee e2 ON e1.supervisorSSN = e2.empSSN
-INNER JOIN tblWorksOn w ON e1.empSSN = w.empSSN
+    INNER JOIN tblEmployee e2 ON e1.supervisorSSN = e2.empSSN
+    LEFT JOIN tblWorksOn w ON e1.empSSN = w.empSSN
 WHERE e2.empName = N'Mai Duy An'
 GROUP BY e1.empSSN,e1.empName;
